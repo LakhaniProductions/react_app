66237a22cc6a6ea541a28a451bbb1f227c876ef5
Adnan Lakhani <adnan@lakhaniproductions.com>
2020-12-18 16:04:13 -0500

    Added Git Ignore file

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -4,12 +4,14 @@ import Lost from './Lost';
 
 const PhotoContainer = props => {
   const results = props.data;
+  
 
   let photos = results.map(photo =>
     <Photo server={photo.photo.server}/>
   );
 
   return (
+   
     <div className="photo-container">
       <h2>Results</h2>
       <ul>
