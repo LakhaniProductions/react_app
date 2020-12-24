183807b33fd8a379c72d1f50987d961f89e5ffe0
Adnan Lakhani <adnan@lakhaniproductions.com>
2020-12-24 16:07:17 -0500

    Browser Router moved to index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter,
-  Route
+  BrowserRouter
 } from 'react-router-dom';
-import PhotoContainer from './components/PhotoContainer';
+
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
- 
     <App />
-
-          <Route path="/cats" render={() => <PhotoContainer data={this.state.cats} title= 'Cat Images'/>} />
-          <Route path="/dogs" render={() => <PhotoContainer data={this.state.dogs} title= 'Dog Images'/>} />
-          <Route path="/computers" render={() => <PhotoContainer data={this.state.computers} title= 'Computers'/>} />
-          <Route path="/:query" render={() => <PhotoContainer data={this.state.images} title= 'Computers'/>} />
-          
-         
-          
-          {
-            (this.state.loading)
-            ?<p>Loading...</p>
-            : <Route exact path="/" render={() => <PhotoContainer data={this.state.images} title= 'Supra Images'/>} />
-          }
-          
-
   </BrowserRouter>
   ,
   document.getElementById('root')
